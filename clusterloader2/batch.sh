ac15e7b062352aa317d2ee25761726caa4683864
Filip Katulski <filip.katulski@cech.ch>
2022-04-05 15:46:45 +0200

    Initial commit - copy of the older tool version

---
+++ clusterloader2/batch.sh
@@ -0,0 +1,8 @@
+./run.sh 1000
+./run.sh 1200
+./run.sh 1400
+./run.sh 1600
+./run.sh 1800
+./run.sh 2000
+./run.sh 2200
+./run.sh 2400
---
+++ clusterloader2/run.sh
@@ -0,0 +1,27 @@
+workers=10
+replicas=3
+extra_flag="_hn"
+sched_qps=800_800
+ctrl_qps=800_800
+
+tag="${sched_qps}_s_${ctrl_qps}_c_${workers}_nodes_${replicas}_pods_typha${extra_flag}"
+
+outdir="$HOME/public/k8s-performance-test"
+reportdir="reports"
+
+mkdir -p "$outdir/$tag"
+
+for h in `seq 1 1`;
+
+do ( 
+clusterloader --provider local  --report-dir "$outdir/$reportdir" --nodes $workers --kubeconfig ~/.kube/config --testconfig ../config/tests/cl2-density-config.yaml --etcd-key /atlas-home/1/avolio/k8s/etcd.p1/etcd-client.key --etcd-certificate /atlas-home/1/avolio/k8s/etcd.p1/etcd-client.crt  --masterip pc-tdq-k8m-master.cern.ch --mastername pc-tdq-k8m-master.cern.ch;
+
+mv "$outdir/$reportdir/junit.xml" "$outdir/$tag/junit_${tag}_${h}.xml";
+mv timeline.csv "$outdir/$tag/timeline_${tag}_${h}.csv";
+
+echo "Sleeping..."
+sleep 180;
+
+);
+
+done
