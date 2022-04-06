e8f59ff184d0f10d1b5f2e11db152763522f4b30
Filip Katulski <filip.katulski@cech.ch>
2022-04-06 14:54:10 +0200

    Gathered additional data for ssh error solving

--- clusterloader2/run.sh
+++ clusterloader2/run.sh
@@ -14,7 +14,9 @@ mkdir -p "$outdir/$tag"
 for h in `seq 1 1`;
 
 do ( 
-clusterloader --provider local  --report-dir "$outdir/$reportdir" --nodes $workers --kubeconfig ~/.kube/config --testconfig ../config/tests/cl2-density-config.yaml --etcd-key /atlas-home/1/avolio/k8s/etcd.p1/etcd-client.key --etcd-certificate /atlas-home/1/avolio/k8s/etcd.p1/etcd-client.crt  --masterip pc-tdq-k8m-master.cern.ch --mastername pc-tdq-k8m-master.cern.ch;
+# clusterloader --provider local  --report-dir "$outdir/$reportdir" --nodes $workers --kubeconfig ~/.kube/config --testconfig ../config/tests/cl2-density-config.yaml --etcd-key /atlas-home/1/avolio/k8s/etcd.p1/etcd-client.key --etcd-certificate /atlas-home/1/avolio/k8s/etcd.p1/etcd-client.crt  --masterip pc-tdq-k8m-master.cern.ch --mastername pc-tdq-k8m-master.cern.ch;
+
+./clusterloader --provider local  --report-dir "$outdir/$reportdir" --nodes 10 --kubeconfig "$HOME/public/config-tbed" --testconfig "$HOME/public/k8s-performance-tests/config/tests/cl2-density-config.yaml" --masterip pc-tbed-k8m-12.cern.ch --mastername pc-tbed-k8m-12.cern.ch
 
 mv "$outdir/$reportdir/junit.xml" "$outdir/$tag/junit_${tag}_${h}.xml";
 mv timeline.csv "$outdir/$tag/timeline_${tag}_${h}.csv";
